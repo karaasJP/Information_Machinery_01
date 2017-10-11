fecf92b1fbd000d74825464f593f08691cc2b346
BuildTools <unconfigured@null.spigotmc.org>
2017-10-11 11:23:00 +0900

    add temp sensor

---
+++ Arduino/sketch_oct11d/sketch_oct11d.ino
@@ -0,0 +1,13 @@
+void setup() {
+Serial.begin(9600) ; // パソコン(ArduinoIDE)とシリアル通信の準備
+}
+void loop() {
+int ans , temp , tv ;
+ans = analogRead(0) ; // アナログ０番ピンからセンサー値を読込む
+tv = map(ans,0,1023,0,5000) ; // センサー値を電圧に変換する
+temp = map(tv,300,1600,-30,100) ; // 電圧から温度に変換する
+Serial.println(ans) ; // ansの値をパソコン(ＩＤＥ)に送る
+Serial.println(tv) ;　// tvの値をパソコンに送る
+Serial.println(temp) ;  // tempの値をパソコンに送る
+delay(1000) ; // １秒毎に繰り返す
+}
