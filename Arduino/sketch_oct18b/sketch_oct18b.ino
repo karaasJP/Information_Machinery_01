9bba61827eb4764b3d5557e51877d7738a81deff
BuildTools <unconfigured@null.spigotmc.org>
2017-10-18 11:09:51 +0900

    add 18b

---
+++ Arduino/sketch_oct18b/sketch_oct18b.ino
@@ -0,0 +1,20 @@
+//加速度センサの値を取得するプログラム
+void setup()
+{
+  Serial.begin(9600) ; // シリアルモニターの初期化をする
+}
+void loop()
+{
+  long x, y, z ;
+  x = y = z = 0 ;
+  x = analogRead(3) ; // Ｘ軸
+  y = analogRead(4) ; // Ｙ軸
+  z = analogRead(5) ; // Ｚ軸
+  Serial.print("X:") ;
+  Serial.print(x) ;
+  Serial.print(" Y:") ;
+  Serial.print(y) ;
+  Serial.print(" Z:") ;
+  Serial.println(z) ;
+  delay(50) ;
+}
