2c4afda2b994a04751bc6546adaf33c43b04243b
naoto107 <ntyg822mtyg@gmail.com>
2017-10-18 03:32:22 -0700

    test3

--- Arduino/sketch_oct11a/sketch_oct11a.ino
+++ Arduino/sketch_oct11a/sketch_oct11a.ino
@@ -4,14 +4,12 @@ Serial.begin(9600) ; // パソコンとシリアル通信の準備を行う
 void loop() {
 double ans ;
 
-aabbcc
 
 ans = analogRead(0) ; // アナログ０番ピンから可変抵抗の値を読み込む
 
 ans = ans*5/1023; //0-1023の値から，電圧の値に変換
 
-printf("Hell")
 
 Serial.println(ans) ; // 読み取った値をパソコン(ＩＤＥ)に送る
-delay(500) ; // 約１秒間隔で処理を繰り返し実行させる
+delay(600) ; // 約１秒間隔で処理を繰り返し実行させる
 }
